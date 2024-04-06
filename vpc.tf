e743912c75946c80032bf6b3e57040ee4866fd26
bertrandmoffo <bertrandmoffo22@gmail.com>
2024-04-05 19:29:48 -0500

    ec2 instance

--- vpc.tf
+++ vpc.tf
@@ -9,3 +9,14 @@ resource "aws_vpc" "main" {
     Name = "bert"
   }
 }
+
+#creating an ec2 instance
+resource "aws_instance" "desamist" {
+  ami           = "ami-032346ab877c418af"
+  instance_type = "t2.micro" 
+  key_name = "moffo key"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
